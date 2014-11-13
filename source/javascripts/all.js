e09b7e65263c0167ed030a7234dbda0a93ec1fcd
miguel michelson <miguelmichelson@gmail.com>
2014-11-13 01:10:45 -0300

    clean deps

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,6 @@
 //= require jquery/dist/jquery.js
 //= require sanitize.js/lib/sanitize.js
 //= require underscore/underscore.js
-//= require backbone/backbone.js
 
 //= require dante/dante
 //= require dante/view
