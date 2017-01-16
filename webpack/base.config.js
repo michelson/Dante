17c9ddbf0c8aace3eaf75356a4ab618172f8c6c9
Miguel Michelson <miguelmichelson@gmail.com>
2017-01-15 23:25:08 -0300

    expose

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,6 +1,13 @@
 import webpack from 'webpack';
 import yargs from 'yargs';
 
+var path = require('path');
+
+var ExtractTextPlugin  = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin  = require('html-webpack-plugin');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+
+
 export const options = yargs
   .alias('p', 'optimize-minimize')
   .alias('d', 'debug')
@@ -19,13 +26,50 @@ const baseConfig = {
 
   externals: undefined,
 
+  resolve: {
+    root: path.resolve('./src'),
+    extensions: ['', '.js', '.jsx', '.es6', '.css', '.scss']
+  },  
+
   module: {
     loaders: [
-      { test: /\.js/, loader: jsLoader, exclude: /node_modules/ }
+      { test: /\.js/, loader: jsLoader, exclude: /node_modules/ },
+      { test: "./src/index.js", loader: "expose-loader?Dante" },
+      { test: "./src/index.js", loader: "expose-loader?DanteEditor" },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file?name=fonts/[name].[ext]',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader:'file?limit=1024&name=images/[name].[ext]'
+      }
+
     ]
   },
 
   plugins: [
+
+    new HtmlWebpackPlugin({
+      title: 'Dante Demo',
+      template: 'demo/assets/index.html',
+      scripts: [
+        {
+          src: '/initialize.js',
+          type: 'module'
+        }
+      ],
+    }),
+  
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(options.optimizeMinimize ? 'production' : 'development')
