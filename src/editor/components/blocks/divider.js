082b86c021c8edd3fcd04f7e90d036d2e5c13ab4
astr0junk <a.ulyanov@hype.ru>
2019-01-24 19:08:15 +0300

    divider in prog

--- src/editor/components/blocks/divider.js
+++ src/editor/components/blocks/divider.js
@@ -2,21 +2,18 @@
 import React from 'react'
 
 export default class DividerBlock extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      enabled: false,
-      data: this.props.blockProps.data.toJS()
-    }
-  }
-
-  componentDidMount() {}
-
-
   render() {
     return (
-      <hr/>
+      <div>
+        <hr/>
+      </div>
     )
   }
 }
 
+export const DividerBlockConfig = (options={})=>{
+  let config = {
+    block: DividerBlock
+  }
+  return Object.assign(config, options)
+}
