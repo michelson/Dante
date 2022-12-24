35e6518c280fd98ae0fa4b86a820c7586e01d633
Dan <44686976+GithubAccountOfDanielDavis@users.noreply.github.com>
2022-12-23 19:39:26 -0500

    Add theme type so styled literals work as expected

--- packages/dante3/src/styled/themes/index.ts
+++ packages/dante3/src/styled/themes/index.ts
@@ -1,4 +1,14 @@
 import darkTheme from "./dark";
 import defaultTheme from "./default";
 
+// This is a bit hacky, but is the least invasive way to make the migration to typescript work
+// Eventually, this could be moved into an explicit interface
+type DanteTheme = typeof defaultTheme | typeof darkTheme
+
+// This is the prescribed method for using custom themes with emotion and typescript
+// @see https://emotion.sh/docs/typescript
+declare module '@emotion/react' {
+  export interface Theme extends DanteTheme {}
+}
+
 export { darkTheme, defaultTheme };
