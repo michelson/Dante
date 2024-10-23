4746f1ae5108d4a13589f2fe7c57cea8cf7f0b6b
Miguel Michelson Martinez <miguelmichelson@gmail.com>
2024-10-22 23:18:51 -0300

    rendered, optional table external

--- packages/dante3/src/styled/base.tsx
+++ packages/dante3/src/styled/base.tsx
@@ -796,64 +796,6 @@ const EditorContainer = styled.div`
         font-weight: 700;
       }
     }
-
-    /* Table-specific styling */
-    table {
-      border-collapse: collapse;
-      margin: 0;
-      overflow: hidden;
-      table-layout: fixed;
-      width: 100%;
-
-      td,
-      th {
-        border: 1px solid var(--gray-1);
-        box-sizing: border-box;
-        min-width: 1em;
-        padding: 6px 8px;
-        position: relative;
-        vertical-align: top;
-
-        > * {
-          margin-bottom: 0;
-        }
-      }
-
-      th {
-        background-color: var(--gray-2);
-        font-weight: bold;
-        text-align: left;
-      }
-
-      .selectedCell:after {
-        background: var(--gray-3);
-        content: "";
-        left: 0; right: 0; top: 0; bottom: 0;
-        pointer-events: none;
-        position: absolute;
-        z-index: 2;
-      }
-
-      .column-resize-handle {
-        background-color: var(--purple);
-        bottom: -2px;
-        pointer-events: none;
-        position: absolute;
-        right: -2px;
-        top: 0;
-        width: 4px;
-      }
-    }
-
-    .tableWrapper {
-      margin: 1.5rem 0;
-      overflow-x: auto;
-    }
-
-    &.resize-cursor {
-      cursor: ew-resize;
-      cursor: col-resize;
-    }
   
   }
   
