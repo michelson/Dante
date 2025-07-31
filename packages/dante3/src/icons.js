e6bcf1d9d617378ed0288f745e3d8733044ae2c8
Christhoval Barba <christhoval06@gmail.com>
2025-07-30 20:38:55 -0500

    Update icons.js

--- packages/dante3/src/icons.js
+++ packages/dante3/src/icons.js
@@ -70,8 +70,7 @@ export function MicOffIcon(props) {
       fill="currentColor"
       viewBox="0 0 24 24"
       aria-hidden="true"
-      tabIndex="-1"
-      title="MicOff"
+      tabIndex={-1}
     >
       <path d="M19 11h-1.7c0 .74-.16 1.43-.43 2.05l1.23 1.23c.56-.98.9-2.09.9-3.28zm-4.02.17c0-.06.02-.11.02-.17V5c0-1.66-1.34-3-3-3S9 3.34 9 5v.18l5.98 5.99zM4.27 3L3 4.27l6.01 6.01V11c0 1.66 1.33 3 2.99 3 .22 0 .44-.03.65-.08l1.66 1.66c-.71.33-1.5.52-2.31.52-2.76 0-5.3-2.1-5.3-5.1H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c.91-.13 1.77-.45 2.54-.9L19.73 21 21 19.73 4.27 3z"></path>
     </svg>
@@ -380,6 +379,13 @@ export function italic() {
   );
 }
 
+
+export function underline() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-underline-icon lucide-underline"><path d="M6 4v6a6 6 0 0 0 12 0V4"/><line x1="4" x2="20" y1="20" y2="20"/></svg>
+  )
+}
+
 export function insertunorderedlist() {
   return (
     <svg
@@ -647,6 +653,24 @@ export function h4() {
   );
 }
 
+
+export function alignLeft() {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-align-left-icon lucide-align-left"><path d="M15 12H3"/><path d="M17 18H3"/><path d="M21 6H3"/></svg>
+)}
+
+export function alignCenter() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-align-center-icon lucide-align-center"><path d="M17 12H7"/><path d="M19 18H5"/><path d="M21 6H3"/></svg>
+  )
+}
+
+export function alignRight() {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-align-right-icon lucide-align-right"><path d="M21 12H9"/><path d="M21 18H7"/><path d="M21 6H3"/></svg>
+  )
+}
+
 export default {
   imageWide,
   imageLeft,
@@ -662,6 +686,7 @@ export default {
   fontColor,
   bold,
   italic,
+  underline,
   insertunorderedlist,
   insertorderedlist,
   link,
@@ -672,4 +697,7 @@ export default {
   h4,
   blockquote,
   code,
+  alignLeft,
+  alignCenter,
+  alignRight,
 };
